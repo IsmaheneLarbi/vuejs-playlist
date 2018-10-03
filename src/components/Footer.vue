8d94fbde3a6bcdcd45fd3b9adce95a1d091cfeb8
meriem <meriemismahlarbi@gmail.com>
2018-10-03 16:43:47 +0200

    props + validation

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <footer>
+          <p>{{copyright}}</p>
+      </footer>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+        copyright:'Copyright of Vue Ninjas',
+    };
+  },
+}
+</script>
+
+<style scoped>
+footer{
+    background: #222;
+    padding: 6px;
+}
+p{
+    color:lightgreen;
+    text-align: center;
+}
+</style>
